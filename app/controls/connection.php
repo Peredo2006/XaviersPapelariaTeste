830e0df6cf0b490db6da25ac1d41f3fb48b4a5e9
Pedro Santos de Oliveira <117773287+Peredo2006@users.noreply.github.com>
2025-09-16 20:55:49 -0300

    Update connection.php

--- app/controls/connection.php
+++ app/controls/connection.php
@@ -1,7 +1,7 @@
 <?php
 $server= "177.136.241.55";
-$user= "talentos_6tech";
-$password= "v,FtYh5n(dkM&(ky";
-$dbname= "talentos_6tech";
+$user= "ifhostgru_6tech";
+$password= "YD3qP-j?Duo5raxZ";
+$dbname= "ifhostgru_6tech";
 $conn = mysqli_connect($server, $user, $password, $dbname);
 ?>
